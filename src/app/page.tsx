831d602d5e0fc4d03900329fb486ce3ada1d3b97
Hannah Hwang <hannahhwang@wifi-10-40-235-9.wifi.berkeley.edu>
2025-06-22 03:51:31 -0700

    map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ const CountyApp: React.FC = () => {
 
   return (
     <div className="flex min-h-screen bg-gray-50">
+      console.log("county", county);
       <CountySidebar
         selectedCounty={selectedCounty}
         onCountySelect={handleCountySelect}
